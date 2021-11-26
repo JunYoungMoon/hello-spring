55562edc3916cafc25c08062d9e502b176eacb20
JunYoung <ko5642027@naver.com>
2021-11-26 22:42:34 +0900

    회원 서비스 개발

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+    //Repository는 단순히 저장소에 넣었다 뺏다 하는 느낌의 메서드명
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
