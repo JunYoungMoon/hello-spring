6cd19ab1df813cec5accf32aea3400bb1255d036
JunYoung <ko5642027@naver.com>
2021-11-28 23:42:32 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     //실무에선 동시성 문제가 있을수 있어서 공유되는 변수에서는 ConcurrentHashMap 사용
