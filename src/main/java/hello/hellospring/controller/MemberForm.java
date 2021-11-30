eacb614405f02d6438f9ad425d2b26be088436d2
JunYoung <ko5642027@naver.com>
2021-11-30 23:55:46 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    //프로퍼티방식
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
