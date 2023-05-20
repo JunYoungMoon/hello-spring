fad8510c71a0165d5a882ee183d8c1974f566719
문준영 <ko5642027@naver.com>
2023-05-20 23:53:14 +0900

    회원 도메인과 리포지토리 만들기

--- src/main/java/hello/hellospring/HelloController.java
+++ src/main/java/hello/hellospring/HelloController.java
@@ -1,9 +1,12 @@
 package hello.hellospring;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +20,19 @@ public class HelloController {
     public String helloMvc(@RequestParam("data") String name, Model model) {
         model.addAttribute("data", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("data") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello{
+        private String name;
     }
 }
